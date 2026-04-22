b791b2f82a3ae243d53c6c869ffcf0274291cc01
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-04-22 00:00:31 +0000

    Connected Database

--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
+import connectDB from './config/mongodb.js';
 
 const app = express();
 const port = process.env.PORT || 4000
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
-app.listen(port,()=> console.log(`Server Started on PORT : ${port}`))
+app.get('/',(req,res)=>res.send("API working fine"));
+
+
+app.listen(port,()=> console.log(`Server Started on PORT : ${port}`));
