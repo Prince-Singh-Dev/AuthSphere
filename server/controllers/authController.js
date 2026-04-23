17a35ae997e7b8686f6a99f40fad041ac05f1690
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-04-23 14:11:13 +0000

    Created User Logout Controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -58,3 +58,16 @@ export const login = async(res,req)=>{
 }
 
 // userLogout Controller 
+export const logout = async (req,res)=>{
+    try{
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV === 'production' ?
+            'none' : 'strict',
+        })
+        return res.json({success:true,message:'Logged Out Successfully'})
+    }catch(error){
+        return res.json({success:false,message:error.message});
+    }
+}
