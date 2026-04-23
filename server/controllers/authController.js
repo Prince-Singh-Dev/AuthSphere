409ee69699b4e0e6d5b93405b10c64720c8099c3
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-04-23 14:07:04 +0000

    Created User Login Controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,9 +23,38 @@ export const register = async(req,res)=>{
             sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge:7*24*60*60*1000
         });
+        return res.json({success:true});
     }catch(error){
         res.json({success:false,message:error.message})
     }
 }
 
 // userLogin Controller
+export const login = async(res,req)=>{
+    const {email,password}=req.body;
+    if(!email || !password){
+        return res.json({success:false,message:'Email and Password are required'})
+    }
+    try{
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false,message:'Invalid Email or Password'})
+        }
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.json({success:false,message:'Invalid Password'})
+        }
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+        res.cookie('token',token ,{
+            httpOnly:true,
+            secure:process.env.NODE_ENV === 'production',
+            sameSite:process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge:7*24*60*60*1000
+        });
+        return res.json({success:true});
+    }catch(error){
+        return res.json({success:false,message:error.message});
+    }
+}
+
+// userLogout Controller 
