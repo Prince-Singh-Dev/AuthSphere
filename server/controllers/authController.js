301ba4a487bc3694bfd1a9a6e97095a051514285
Prince-Singh-Dev <princesingh6173@gmail.com>
2026-04-25 06:36:31 +0000

    Created API to check whether user is already authenticated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -109,7 +109,6 @@ export const sendVerifyOtp = async (req,res)=>{
 }
 
 // Verifying OTP controller
-
 export const verifyEmail = async (req,res)=>{
     const {userId,otp}=req.body;
     if(!userId || !otp){
@@ -133,5 +132,15 @@ export const verifyEmail = async (req,res)=>{
         return res.json({success:true,message:'Account Verified SuccessFully'})
     }catch(error){
         return res.json({success:false,message:error.message});
+    }
+}
+
+// user Already authenticated controller
+export const isAuthenticated = async(req,res)=>{
+    try{
+
+        return res.json({success:true});
+    }catch(error){
+        res.json({success:false,message:error.message});
     }
 }
